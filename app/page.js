b56844dc66eb611fc6a98c85a56e06253a50e9c4
kpdevSE <kanishkapasindu6@gmail.com>
2024-05-29 20:51:40 +0530

    testing

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Navigation from "./components/mainNavigation";
+
+export default function App() {
+  return (
+    <div className="w-[85%] mx-auto">
+      <Navigation />
+    </div>
+  );
+}
